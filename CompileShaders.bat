f1806c566c127fad6ebcdc01271b5e8df586cd27
gleb-kaistro <gleb.kaistro@gmail.com>
2025-07-01 15:51:10 +0200

    hello vulkan

---
+++ CompileShaders.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Get the directory of the running batch file
+set "BASE_DIR=%~dp0"
+set "GLSLC=c:\VulkanSDK\1.4.309.0\Bin\glslc.exe"
+set "INPUT_DIR=%BASE_DIR%Delta\Shaders"
+set "OUTPUT_DIR=%BASE_DIR%Resources\Shaders"
+
+for %%f in ("%INPUT_DIR%\*") do (
+    set "filename=%%~nxf.spv"
+    "%GLSLC%" "%%f" -o "%OUTPUT_DIR%\!filename!"
+)
+
+echo Done.
+pause
